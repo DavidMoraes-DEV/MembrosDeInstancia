ee0ef67d57ce02e5f95c4e84b6735e4941fe59d4
David Moraes <investtraderrs@gmail.com>
2020-09-16 19:08:47 -0300

    Membros de Instancia

---
+++ ExemploDeMembrosDeInstancia/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExemploDeMembrosDeInstancia
+{
+    class Calculadora
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public static double Pi = 3.14; //Membro estárico
+
+        public static double Circunferencia(double r) //Membro Estatico
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double Volume(double r)  //Membro Estatico
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ ExemploDeMembrosDeInstancia/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExemploDeMembrosDeInstancia;
+using System;
+using System.Globalization;
+
+namespace ExemploDeMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CI);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
+
+            Console.WriteLine();
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CI));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CI));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CI));
+        }
+    }
+}
+
+/*Neste exemplo foi utilizado os membros de INSTÂNCIA com a crianção de uma nova
+ CLASSE chamada Calculadora para realiza os cálculos separadamente do programa principal.
+Utilizando depois membro estáticos para conseguir chamar os métodos diretamento pelo nome da classe
+ao invés de instânciar um objeto para utiliza-lo em sua chamada*/
